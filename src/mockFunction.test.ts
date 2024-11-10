bd2139bb4260f3ef7570df049a998bf90927a0cf
znehraks <znehraks@gmail.com>
2024-11-10 22:29:14 +0900

    test: skip, todo

--- src/mockFunction.test.ts
+++ src/mockFunction.test.ts
@@ -1,16 +1,24 @@
 import { obj } from './mockFucntion';
 
 // let spyFn: jest.SpyInstance;
-test('obj.minus 메소드가 1번 호출되었다.(spy 삽입)', () => {
-  jest.spyOn(obj, 'minus');
-  const result = obj.minus(1, 2);
-  expect(obj.minus).toHaveBeenCalledTimes(1);
-  expect(result).toBe(-1);
-});
-test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 실행은 안되게)', () => {
-  jest.spyOn(obj, 'minus').mockImplementation();
-  const result = obj.minus(1, 2);
-  expect(obj.minus).toHaveBeenCalledTimes(1);
+
+describe('beforeEach, afterEach 적용', () => {
+  beforeEach(() => {});
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  test('obj.minus 메소드가 1번 호출되었다.(spy 삽입)', () => {
+    jest.spyOn(obj, 'minus');
+    const result = obj.minus(1, 2);
+    expect(obj.minus).toHaveBeenCalledTimes(1);
+    expect(result).toBe(-1);
+  });
+  test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 실행은 안되게)', () => {
+    jest.spyOn(obj, 'minus').mockImplementation();
+    const result = obj.minus(1, 2);
+    expect(obj.minus).toHaveBeenCalledTimes(1);
+  });
 });
 
 test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 구현 mock)', () => {
@@ -30,29 +38,22 @@ test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 구현 
   // spyFn.mockRestore(); // 아예 전부 없앰
 });
 
-test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 return 값만 바꾸기)', () => {
+test.skip('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 return 값만 바꾸기)', () => {
   jest.spyOn(obj, 'minus').mockReturnValue(5);
   const result = obj.minus(1, 2);
   expect(result).toBe(5);
 });
 
-test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 return 값만 한번 바꾸기)', () => {
+test.skip('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 return 값만 한번 바꾸기)', () => {
   jest.spyOn(obj, 'minus').mockReturnValueOnce(5).mockReturnValueOnce(2);
   const result = obj.minus(1, 2);
   expect(result).toBe(5);
 });
 
+test.todo('나중에 만들거야!');
+
 beforeAll(() => {
   console.log('이 파일 준비사항');
-});
-
-beforeEach(() => {
-  console.log('각 테스트 전');
-});
-
-afterEach(() => {
-  console.log('각 테스트 후');
-  jest.restoreAllMocks();
 });
 
 afterAll(() => {
