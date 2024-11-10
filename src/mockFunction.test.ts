f26b785f13737c88e77f9fac1f866a3b8c254e5e
znehraks <znehraks@gmail.com>
2024-11-10 22:14:47 +0900

    test: 공통 부분 처리

--- src/mockFunction.test.ts
+++ src/mockFunction.test.ts
@@ -1,35 +1,33 @@
 import { obj } from './mockFucntion';
 
+// let spyFn: jest.SpyInstance;
 test('obj.minus 메소드가 1번 호출되었다.(spy 삽입)', () => {
-  const spyFn = jest.spyOn(obj, 'minus');
+  jest.spyOn(obj, 'minus');
   const result = obj.minus(1, 2);
   expect(obj.minus).toHaveBeenCalledTimes(1);
   expect(result).toBe(-1);
-  spyFn.mockRestore();
 });
 test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 실행은 안되게)', () => {
-  const spyFn = jest.spyOn(obj, 'minus').mockImplementation();
+  jest.spyOn(obj, 'minus').mockImplementation();
   const result = obj.minus(1, 2);
   expect(obj.minus).toHaveBeenCalledTimes(1);
-  spyFn.mockRestore();
 });
 
 test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 구현 mock)', () => {
   jest.spyOn(obj, 'minus').mockImplementation(() => 5);
-
   const result = obj.minus(1, 2);
   expect(result).toBe(5);
 });
 test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 구현 mock 한번씩만)', () => {
-  const spyFn = jest
+  jest
     .spyOn(obj, 'minus')
     .mockImplementationOnce(() => 5)
     .mockImplementationOnce(() => 2);
   const result = obj.minus(1, 2);
   expect(result).toBe(5);
-  spyFn.mockClear(); // Times, With 초기화
-  spyFn.mockReset(); // mockClear + mockImplementation(() => {})
-  spyFn.mockRestore(); // 아예 전부 없앰
+  // spyFn.mockClear(); // Times, With 초기화
+  // spyFn.mockReset(); // mockClear + mockImplementation(() => {})
+  // spyFn.mockRestore(); // 아예 전부 없앰
 });
 
 test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 return 값만 바꾸기)', () => {
@@ -42,4 +40,21 @@ test('obj.minus 메소드가 1번 호출되었다.(spy는 심고, 함수 return 
   jest.spyOn(obj, 'minus').mockReturnValueOnce(5).mockReturnValueOnce(2);
   const result = obj.minus(1, 2);
   expect(result).toBe(5);
+});
+
+beforeAll(() => {
+  console.log('이 파일 준비사항');
+});
+
+beforeEach(() => {
+  console.log('각 테스트 전');
+});
+
+afterEach(() => {
+  console.log('각 테스트 후');
+  jest.restoreAllMocks();
+});
+
+afterAll(() => {
+  console.log('이 파일 마무리');
 });
